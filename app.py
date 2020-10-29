47dd911248c9b560d84854c33f28edefd3c934e6
wangyun <dianawangyun@gmail.com>
2020-10-29 01:35:37 -0500

    added user login

--- app.py
+++ app.py
@@ -1,25 +1,156 @@
-from flask import Flask, render_template, request, jsonify
+import os
+
+from flask import Flask, render_template, request, jsonify, flash, redirect, session, g
 from flask import session, make_response
+
+from sqlalchemy.exc import IntegrityError
 import requests
+
+from forms import UserAddForm, LoginForm, EditProfileForm
+from models import db, connect_db, User, Song, Favors
+
+CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "hellosecret123"
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgres:///music-box'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 API_BASE_URL = "https://itunes.apple.com/search"
+
+connect_db(app)
+# db.drop_all()
+# db.create_all()
+
+
+@app.before_request
+def add_user_to_g():
+    """If logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup.
+
+    Create new user and add to DB. Redirect to home page.
+
+    If form not valid, present form.
+
+    If the there already is a user with that username: flash message
+    and re-present form.
+    """
+
+    form = UserAddForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.signup(
+                username=form.username.data,
+                password=form.password.data,
+                email=form.email.data,
+            )
+            db.session.commit()
+
+        except IntegrityError:
+            flash("Username already taken", 'danger')
+            return render_template('signup.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('signup.html', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Welcome back, {user.username}!", "success")
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('login.html', form=form)
+
+
+@app.route("/logout")
+def logout():
+    """Handle logout of user."""
+
+    do_logout()
+    flash("You have successfully logged out.", "success")
+    return redirect('/')
 
 
 @app.route("/", methods=["GET", "POST"])
-def show_resp():
+def show_search_page():
     if request.method == 'POST':
         req = request.json
         res = requests.get(API_BASE_URL, params=req).json().get("results")
-        # import pdb
-        # pdb.set_trace()
         s_res = [serialize_response(item) for item in res]
-        session["searchRes"] = s_res
         return jsonify(s_res)
     else:
-        return render_template("home.html")
+        return render_template("search.html", user=g.user)
+
+
+@app.route("/fav", methods=["POST"])
+def toggle_fav():
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
+    user = User.query.get_or_404(g.user.id)
+    req = request.json
+    track_id = req.get("trackId")
+    favs_id = [fav.song_id for fav in g.user.favors]
+
+    if track_id not in favs_id:
+        song = Song(**req)
+        if not Song.query.filter_by(trackId=song.trackId).first():
+            db.session.add(song)
+            db.session.commit()
+        fav = Favors(user_id=g.user.id, song_id=song.trackId)
+        db.session.add(fav)
+        db.session.commit()
+        return jsonify({'message': 'successfully added'})
+    else:
+        fav = Favors.query.filter_by(song_id=track_id, user_id=user.id).first()
+        # import pdb
+        # pdb.set_trace()
+        user.favors.remove(fav)
+        db.session.commit()
+        return jsonify({'message': 'successfully deleted'})
 
 
 def serialize_response(res):
@@ -29,22 +160,22 @@ def serialize_response(res):
     # pdb.set_trace()
     return {
         'kind': res.get("kind"),
-        'primaryGenreName': res.get("primaryGenreName"),
-        'collectionId': res.get("collectionId"),
-        'collectionName': res.get("collectionName"),
-        'trackCount': res.get("trackCount"),
-        'trackViewUrl': res.get("trackViewUrl"),
-        'trackId': res.get("trackId"),
-        'trackName': res.get("trackName"),
-        'trackNumber':res.get('trackNumber'),
-        'artistId': res.get("artistId"),
-        'artistName': res.get("artistName"),
-        'artistViewUrl': res.get("artistViewUrl"),
-        'previewUrl': res.get("previewUrl"),
+        'primaryGenreName': res.get("primaryGenreName", "N/A"),
+        'collectionId': res.get("collectionId", "N/A"),
+        'collectionName': res.get("collectionName", "N/A"),
+        'trackCount': res.get("trackCount", "N/A"),
+        'trackViewUrl': res.get("trackViewUrl", "N/A"),
+        'trackId': res.get("trackId", "N/A"),
+        'trackName': res.get("trackName", "N/A"),
+        'trackNumber': res.get("trackNumber", "N/A"),
+        'artistId': res.get("artistId", "N/A"),
+        'artistName': res.get("artistName", "N/A"),
+        'artistViewUrl': res.get("artistViewUrl", "N/A"),
+        'previewUrl': res.get("previewUrl", "N/A"),
         'artworkUrl': artworkUrl,
-        'releaseDate': res.get("releaseDate"),
-        'collectionPrice': res.get("collectionPrice"),
-        'trackPrice': res.get("trackPrice"),
-        'currency': res.get("currency"),
-        'country': res.get("country")
+        'releaseDate': res.get("releaseDate", "N/A"),
+        'collectionPrice': res.get("collectionPrice", "N/A"),
+        'trackPrice': res.get("trackPrice", "N/A"),
+        'currency': res.get("currency", "N/A"),
+        'country': res.get("country", "N/A")
     }
--- static/classes.js
+++ static/classes.js
@@ -22,84 +22,146 @@ class Song {
         this.country = item.country;
     }
 
-    DrawDetailPage() {}
+    drawDetailPage() {
+        $("#detail-cover").attr("src", this.artworkUrl);
+        let detailTable = $("<table/>").addClass("table table-hover");
+        let detailBody = $("<tbody/>").html(`
+  <tr>
+  <td>Song</td><td>${this.trackName}</td></tr>
+  <tr><td>Artist</td><td><a href="${this.artistViewUrl}" target="_blank">${
+            this.artistName
+        }</a></td></tr>
+    <tr><td>Genre</td><td>${this.primaryGenreName}</td></tr>
+    <tr><td>Album</td><td>${this.collectionName}</td></tr>
+    <tr><td>Total Tracks</td><td>${this.trackCount}</td></tr>
+    <tr><td>Track# of this Song</td><td>${this.trackNumber}</td></tr>
+    <tr><td>Release Date</td><td>${this.releaseDate.slice(0, 10)}</td></tr>
+    <tr><td>Release Country</td><td>${this.country}</td></tr>
+    <tr><td>Album Price</td><td>${this.collectionPrice} ${
+            this.currency
+        }</td></tr>
+    <tr><td>Track Price</td><td>${this.trackPrice} ${this.currency}</td></tr>
+    <tr><td>Check on Itunes</td><td><a href="${
+        this.trackViewUrl
+    }" target="_blank">Click Here</a></td></tr>
+  `);
+        detailTable.append(detailBody);
+        $("#detail-wrapper").html(detailTable);
+        $("#detail-modal").modal("show");
+    }
+
+    showAddFavorBtn($li) {
+        if ($("#logout").length > 0) {
+            let favBtn = $("<button/>", {
+                type: "button",
+                class: "btn btn-outline-danger favor",
+                text: "I Like This!",
+                click: () => {
+                    this.toggleFav();
+                },
+            });
+            $li.find(".card-body").append(favBtn);
+        }
+    }
+
+    async toggleFav() {
+        const response = await axios.post(`${BASE_URL}/fav`, {
+            kind: this.kind,
+            primaryGenreName: this.primaryGenreName,
+            collectionId: this.collectionId,
+            collectionName: this.collectionName,
+            trackCount: this.trackCount,
+            trackViewUrl: this.trackViewUrl,
+            trackId: this.trackId,
+            trackName: this.trackName,
+            trackNumber: this.trackNumber,
+            artistId: this.artistId,
+            artistName: this.artistName,
+            artistViewUrl: this.artistViewUrl,
+            previewUrl: this.previewUrl,
+            artistViewUrl: this.artistViewUrl,
+            artworkUrl: this.artworkUrl,
+            releaseDate: this.releaseDate,
+            collectionPrice: this.collectionPrice,
+            trackPrice: this.trackPrice,
+            currency: this.currency,
+            country: this.country,
+        });
+        console.log(response);
+    }
 }
 
 class SongMusic extends Song {
     constructor(item) {
         super(item);
-        this.element = $("<li/>").html(`
-        <div class="card" style="width: 20rem;" class="mr-2 mb-2">
-        <div class="player-wrapper">
-          <img src="${this.artworkUrl}" class="card-img-top img-fluid max-height:100% rounded" alt="Album Cover">
-          <div class="cover">
-            <div class="cover-icon play"></div>
-          </div>
-        </div>
-        <div class="card-body">
-          <h5 class="card-title">${this.trackName}</h5>
-          <p class="card-text">Album: ${this.collectionName}</p>
-          <p class="card-text">Artist: ${this.artistName}</p>
-          <button type="button" class="btn btn-primary mr-4 detail">Check Details</button>
-          <button type="button" class="btn btn-outline-danger favor">I Like This!</button>
-        </div>
-      </div>`);
+        this.element = this.createElement();
         this.element.find(".cover").click(() => {
-            let $audio = $("#audio-player");
-            let $icon = this.element.find(".cover-icon");
-            $audio.attr("src", this.previewUrl);
-            $audio[0].onended = function () {
-                $icon.removeClass("pause").addClass("play");
-            };
-            if ($icon.hasClass("play")) {
-                $audio[0].play();
-                $(".cover-icon").removeClass("pause").addClass("play");
-                $icon.removeClass("play").addClass("pause");
-            } else {
-                $audio[0].pause();
-                $icon.removeClass("pause").addClass("play");
-            }
+            this.initPlayer();
         });
-
         this.element.find(".detail").click(() => {
-            $("#detail-cover").attr("src", this.artworkUrl);
-            let detailTable = $("<table/>").addClass("table table-hover");
-            let detailBody = $("<tbody/>").html(`
-          <tr>
-          <td>Song</td><td>${this.trackName}</td></tr>
-          <tr><td>Artist</td><td><a href="${
-              this.artistViewUrl
-          }" target="_blank">${this.artistName}</a></td></tr>
-            <tr><td>Genre</td><td>${this.primaryGenreName}</td></tr>
-            <tr><td>Album</td><td>${this.collectionName}</td></tr>
-            <tr><td>Total Tracks</td><td>${this.trackCount}</td></tr>
-            <tr><td>Track# of this Song</td><td>${this.trackNumber}</td></tr>
-            <tr><td>Release Date</td><td>${this.releaseDate.slice(
-                0,
-                10
-            )}</td></tr>
-            <tr><td>Release Country</td><td>${this.country}</td></tr>
-            <tr><td>Album Price</td><td>${this.currency} ${
-                this.collectionPrice
-            }</td></tr>
-            <tr><td>Track Price</td><td>${this.currency} ${
-                this.trackPrice
-            }</td></tr>
-            <tr><td>Check on Itunes</td><td><a href="${
-                this.trackViewUrl
-            }" target="_blank">Click Here</a></td></tr>
-          `);
-            detailTable.append(detailBody);
-            $("#detail-wrapper").html(detailTable);
-            $("#detail-modal").modal("show");
+            this.drawDetailPage();
         });
+    }
+    createElement() {
+        let $li = $("<li/>")
+            .html(
+                `
+    <div class="card" style="width: 20rem;" class="mr-2 mb-2">
+    <div class="player-wrapper">
+      <img src="${this.artworkUrl}" class="card-img-top img-fluid max-height:100% rounded" alt="Album Cover">
+      <div class="cover">
+        <div class="cover-icon play"></div>
+      </div>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">${this.trackName}</h5>
+      <p class="my-2">Artist: ${this.artistName}</p>
+      <p class="mb-4">Album: ${this.collectionName}</p>
+      <button type="button" class="btn btn-primary mr-4 detail">Check Details</button>
+    </div>
+  </div>`
+            )
+            .addClass("mb-4");
+        this.showAddFavorBtn($li);
+        return $li;
+    }
+
+    initPlayer() {
+        let $audio = $("#audio-player");
+        let $icon = this.element.find(".cover-icon");
+        $audio.attr("src", this.previewUrl);
+        $audio[0].onended = function () {
+            $icon.removeClass("pause").addClass("play");
+        };
+        if ($icon.hasClass("play")) {
+            $audio[0].play();
+            $(".cover-icon").removeClass("pause").addClass("play");
+            $icon.removeClass("play").addClass("pause");
+        } else {
+            $audio[0].pause();
+            $icon.removeClass("pause").addClass("play");
+        }
     }
 }
 
-class SongVideo extends SongMusic {
+class SongVideo extends Song {
     constructor(item) {
         super(item);
-        this.element = $("<li>").html(`
+        this.element = this.createElement();
+
+        this.element.find(".detail").click(() => {
+            this.drawDetailPage();
+        });
+
+        this.element.find(".player-wrapper").click(() => {
+            this.initPlayer();
+        });
+    }
+
+    createElement() {
+        let $li = $("<li>")
+            .html(
+                `
     <div class="card" style="width: 20rem;" class="mr-2">
       <div class="player-wrapper">
         <img src="${this.artworkUrl}" class="card-img-top" alt="Album Cover">
@@ -109,28 +171,31 @@ class SongVideo extends SongMusic {
       </div>
       <div class="card-body">
         <h5 class="card-title">${this.trackName}</h5>
-        <p class="card-text">Album: ${this.collectionName}</p>
-        <p class="card-text">Artist: ${this.artistName}</p>
+        <p class="my-2">Artist: ${this.artistName}</p>
+        <p class="mb-4">Album: ${this.collectionName}</p>
         <button type="button" class="btn btn-primary mr-4 detail">Check Details</button>
-        <button type="button" class="btn btn-outline-danger favor">I Like This!</button>
       </div>
-    </div>`);
+    </div>`
+            )
+            .addClass("mb-4");
+        this.showAddFavorBtn($li);
+        return $li;
+    }
 
-        this.element.find(".player-wrapper").click(() => {
-            $("#video-title").text(`${this.trackName} - ${this.artistName}`);
-            if (this.previewUrl) {
-                $("#video-player").attr("src", this.previewUrl);
-                $("#video-info").text("");
-            } else {
-                $("#video-player").attr("src", "");
-                $("#video-info").text(
-                    "Oops, there is no preview for this music video..."
-                );
-            }
-            $("#video-modal").modal("show");
-            $("#video-modal").on("hidden.bs.modal", function () {
-                $("#video-player")[0].pause();
-            });
+    initPlayer() {
+        $("#video-title").text(`${this.trackName} - ${this.artistName}`);
+        if (this.previewUrl !== "N/A") {
+            $("#video-player").attr("src", this.previewUrl);
+            $("#video-info").text("");
+        } else {
+            $("#video-player").attr("src", "");
+            $("#video-info").text(
+                "Oops, there is no preview for this music video..."
+            );
+        }
+        $("#video-modal").modal("show");
+        $("#video-modal").on("hidden.bs.modal", function () {
+            $("#video-player")[0].pause();
         });
     }
 }
